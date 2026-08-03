81372b9d32b20da616e29a75f06e56b24d284e6c
Abishai M <abimxai@gmail.com>
2026-08-03 15:31:57 +0300

    initial changes

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "aws_vpc" "name" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+      Name = "abi_test_vpp"
+    }
+  
+}
+
+
+resource "aws_subnet" "name2" {
+    vpc_id = aws_vpc.name.id
+    cidr_block = "10.0.0.0/24"
+    tags = {
+      Name = "abi_test_subnet"
+    }
+  
+}
