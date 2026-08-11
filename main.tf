6e8093fb633ee20379089b4f3feba5eab6bf32e7
Abishai <142227390+abishaix@users.noreply.github.com>
2026-08-11 23:37:23 +0300

    Add tags to aws_subnet resource in main.tf

--- main.tf
+++ main.tf
@@ -10,8 +10,12 @@ resource "aws_vpc" "name" {
 resource "aws_subnet" "name1" {
     vpc_id = aws_vpc.name.id
     cidr_block = "10.0.0.0/24"
+
     tags = {
       Name = "abi_test_subnet"
+      OS   = "amazonlinux"
+      Environment = "dev"
+        
     }
   
 }
